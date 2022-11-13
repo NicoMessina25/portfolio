1e5d0a29eddda40151ab52e36c602f05ec5c187b
Nicolas Messina <nicolasmessina25@gmail.com>
2022-11-13 14:15:49 -0300

    Se implemento la seccion de tecnologias conocidas

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 
 async function loadProjects(arrProj){
-    const response = await fetch("../js/projects.json");
+    const response = await fetch("./js/projects.json");
     const projs = await response.json();
     projs.forEach((p)=>{
         arrProj.push(p);
@@ -10,7 +10,7 @@ async function loadProjects(arrProj){
 }
 
 async function loadTechs(arrTech){
-    const response = await fetch("../js/technologies.js");
+    const response = await fetch("./js/technologies.json");
     const techs = await response.json();
     techs.forEach((t)=>{
         arrTech.push(t);
@@ -18,8 +18,14 @@ async function loadTechs(arrTech){
 }
 
 const divProjects = document.getElementById("divProjects");
+const divTechs = document.getElementById("divTechnologies");
 const arrProj = new Array();
 const arrTech = new Array();
+/* const arrButtons = new Array();
+arrButtons.push(document.getElementById("menuItemMe"));
+arrButtons.push(document.getElementById("menuItemProjects"));
+arrButtons.push(document.getElementById("menuItemTechnologies"));
+arrButtons.push(document.getElementById("menuItemContact")); */
 
 loadProjects(arrProj).then(()=>{
     arrProj.forEach(({name, img, link, git})=>{
@@ -40,3 +46,42 @@ loadProjects(arrProj).then(()=>{
         `
     })
 });
+
+loadTechs(arrTech).then(()=>{
+    arrTech.forEach(({name, img})=>{
+        const nameArray = name.split(" ");
+        const title = document.createElement("h3");
+        
+        nameArray.forEach((word)=>{
+            title.innerHTML += ` <span>${word.charAt(0)}</span>${word.slice(1)}`;
+        });
+
+        divTechs.innerHTML += `
+            <article class="technology flexible--column"> 
+                <h3 class="title"> ${title.innerHTML} </h3>
+                <img src="${img}" alt="">
+            </article>
+        `
+    })
+
+});
+
+/* arrButtons.forEach((b)=>{
+    let hFrom = window.scrollY;
+    let hTo = document.getElementById(`section${b.getAttribute("id").slice(8)}`).offsetTop;
+    let nIntervalId
+
+    b.addEventListener("click", ()=>{
+        if(hFrom - hTo != 0){
+            nIntervalId = setInterval(()=>{
+                    if(window.scrollY != hTo){
+                        window.scrollBy(0,(window.scrollY > hTo)?-1:1);
+                    } else {
+                        clearInterval(nIntervalId);
+                    }
+                },100/Math.abs(hFrom-hTo))
+        }
+    });
+    
+   
+}); */
